a913f49f0c7a90c48c18bb8a07e73e248f9951a3
Jacob Wanzenried <62063187+wanzenried@users.noreply.github.com>
2024-11-01 13:15:11 +0100

    added the basic snake functionality

--- Snake/src/main.cpp
+++ Snake/src/main.cpp
@@ -1,24 +1,61 @@
 #include <Arduino.h>
 #include <VT100.h>
 
+// Prototypes:
+void doGameEngine();
+void handleUser();
+void pushSnake(int x, int y);
+void updateSnake();
+
+// Definitions
+#define snakeMaxLen 20
+
+
 // period time for game engine
 #define TIMEPER 200
 unsigned long gameTime;
 
-int sign = 1;
+int dirX = 1;
+int dirY = 0;
+
+typedef struct
+{
+  int x;
+  int y;
+} snakePart;
+
+
+int snakeLen = 0;
+
+snakePart snakeArr[snakeMaxLen];
 
 
 
+void pushSnake(int x, int y) {
+  snakePart s = {x, y};
+
+  snakeArr[snakeLen] = s;
+
+  snakeLen++;
+}
+
+void updateSnake() {
+  snakeArr[0].x += dirX;
+  snakeArr[0].y += dirY;
+
+  for (int i = snakeLen-1; i > 0; i--)
+  {
+    snakeArr[i] = snakeArr[i-1];
+  }
+  
+}
 
 
 
-
-// gameengine
-int counter1 = 0;
-int counter2 = 0;
 
 void doGameEngine() {
   saveCursor();
+  /*
   counter1 += sign;
   counter2 -= sign;
   //cursorOff();
@@ -29,14 +66,20 @@ void doGameEngine() {
 
   setCursor(11, 10);
   eraseToEOL();
-  sp(counter2);
+  sp(counter2);*/
+  updateSnake();
 
+  setCursor(snakeArr[0].x,snakeArr[0].y);
+  sp("#");
+
+  eraseCh(snakeArr[snakeLen-1].x, snakeArr[snakeLen-1].y);
   restoreCursor();
   //cursorOn();
 }
 
 void handleUser() {
   int k;
+  int tmp;
   // basicly cehck if keyboard input and process it
   // NB NB in gtkTerm and putty no <enter> is needed
   // keys are transmitted at once
@@ -44,14 +87,18 @@ void handleUser() {
   while (Serial.available()) {
     k = Serial.read();  // one byte 0..255
     switch (k) {
-      case '+':
-        sign = 1;
+      case 'a':
+        tmp = dirY;
+        dirY = dirX;
+        dirX = -1*tmp;
         break;
-      case '-':
-        sign = -1;
+      case 'd':
+        tmp = dirX;
+        dirX = dirY;
+        dirY = -1*tmp;
         break;
       case 'c':
-        counter1 = counter2 = 0;
+        
         break;
       default:;
     }
@@ -64,13 +111,19 @@ void setup() {
   Serial.begin(115200);
   Serial.println(__DATE__);
   Serial.println(__FILE__);
-  delay(200);
+  delay(500);
   clrScreen();
   screenWonB();
   setCursor(0, 0);
   cursorOff();
 
   gameTime = millis() + TIMEPER;
+
+  for (int i = 0; i < 8; i++)
+  {
+    pushSnake(14,14);
+  }
+  
 }
 
 unsigned long nextTime;
